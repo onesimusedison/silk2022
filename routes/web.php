147b7051bcc03f6dd18eb881144481e4e2aaa66e
onesimusedison <onesimusedison41@gmail.com>
2022-03-29 02:40:48 +0700

    1

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome_silk');
+});
+
+Route::get('/generatepassword','App\Http\Controllers\LoginController@generatepassword');
+Route::post('/validasilogin','App\Http\Controllers\LoginController@validasiLogin');
+Route::get('/dash','App\Http\Controllers\AdminController@index');
+Route::get('/logout','App\Http\Controllers\LoginController@logoutSistem');
